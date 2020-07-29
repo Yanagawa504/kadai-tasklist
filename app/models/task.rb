76dad609939e2b127f756c760c670289c64463b8
Asaki.Yanagawa <yanagawa.asaki@imagicalab.co.jp>
2020-07-29 05:51:51 +0000

    サインアップ終了

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+    #belong_to :user
+    #今はまだその時ではない
     validates :content, presence: true, length: { maximum: 255 }
     validates :status, presence: true, length: { maximum: 10 }
 end
